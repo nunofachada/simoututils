d541bc4e495be55d21d679d66e4cf4f0aa8412f1
Nuno Fachada <nfachada@laseeb.org>
2015-12-14 19:47:57 +0000

    Encompass pp_plot_many into output_plot and make it generic (wip)

--- simout/output_plot.m
+++ simout/output_plot.m
@@ -1,13 +1,18 @@
-function output_plot(file, outputs, layout, scale, colors)
+function output_plot(...
+    folder, file, outputs, type, layout, scale, colors, iters)
 % OUTPUT_PLOT Plot time-series simulation output.
 %
-%   OUTPUT_PLOT(file, outputs, layout, scale, colors)
+%   OUTPUT_PLOT(folder, file, outputs, type, layout, scale, colors, iters)
 %
 % Parameters:
 %        file - file containing output of one simulation run.
 %     outputs - Either an integer representing the number of outputs in 
 %               each file or a cell array of strings with the output names.
 %               In the former case, output names will be 'o1', 'o2', etc.
+%        type - Type of plot:
+%               'a' - Superimposed outputs (default).
+%               'f' - Plot filled area encompassed by output extremes.
+%               0...w - Moving average, window size w
 %      layout -
 %       scale -
 %      colors -
@@ -17,31 +22,52 @@ function output_plot(file, outputs, layout, scale, colors)
 % at http://opensource.org/licenses/MIT)
 %
 
-% Read file containing outputs
-data = dlmread(file);
+% Read files containing outputs
+listing = dir([folder '/' files]);
+num_files = size(listing, 1);
+
+% Were any files found?
+if num_files == 0
+    error('No files found.');
+end;
+
+% Load data from first file (used for defaults)
+data = dlmread([folder '/' listing(1).name]);
 
 % Default output names if respective arguments not given
-if nargin < 2
+if nargin < 3
     num_outputs = size(data, 2);
     outputs = parse_output_names(num_outputs);
 else
     [outputs, num_outputs] = parse_output_names(outputs);
 end;
 
+% Default type
+if nargin < 4
+    type = 'a';
+end;
+
 % Default layout
-if nargin < 3
+if nargin < 5
     layout = num_outputs;
 end;
 
 % Default scale
-if nargin < 4
+if nargin < 6
     scale = ones(1, num_outputs);
 elseif numel(scale) == 1
     scale = scale * ones(1, num_outputs);
 end;
 
 % Default colors
-if nargin < 5, colors = {'b', 'r', 'g', 'c', 'm', 'y', 'k'}; end;
+if nargin < 7
+    colors = {'b', 'r', 'g', 'c', 'm', 'y', 'k'}; 
+end;
+
+% Default number of iterations
+if nargin < 8
+    iters = size(data, 1);
+end;
 
 % Start plotting outputs
 i1 = 1;
@@ -54,7 +80,13 @@ for l = layout
     grid on;
     
     for i = i1:i2
-        plot(data(:, i) * scale(i), colors{i - i1 + 1});
+        
+        for j = 1:numFiles
+            
+            data = dlmread([folder '/' listing(j).name]);
+            plot(data(:, i) * scale(i), colors{i - i1 + 1});
+            
+        end;    
     end;
 
     xlim([0 size(data, 1)]);
