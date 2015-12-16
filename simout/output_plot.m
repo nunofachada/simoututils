cf2424f5baf73d4e1d73db20e94fe48d729745b7
Nuno Fachada <nfachada@laseeb.org>
2015-12-16 20:37:12 +0000

    output_plot: proper doccomments + small code improvements

--- simout/output_plot.m
+++ simout/output_plot.m
@@ -1,8 +1,10 @@
-function d = output_plot(...
-    folder, files, outputs, type, layout, scale, colors, iters)
-% OUTPUT_PLOT Plot time-series simulation output.
+function [d, h] = output_plot(...
+    folder, files, outputs, type, layout, scale, iters, colors)
+% OUTPUT_PLOT Plot time-series simulation output from one or more 
+% replications using one of three approaches: superimposed, extremes or
+% moving average.
 %
-%   OUTPUT_PLOT(folder, file, outputs, type, layout, scale, colors, iters)
+%   [d, h] = OUTPUT_PLOT(folder, file, outputs, type, layout, scale, iters, colors)
 %
 % Parameters:
 %      folder - Folder containing simulation output files.
@@ -14,19 +16,32 @@ function d = output_plot(...
 %        type - Type of plot:
 %               'a' - Superimposed outputs (default).
 %               'f' - Plot filled area encompassed by output extremes.
-%               0...w - Moving average, window size w
-%      layout -
-%       scale -
-%      colors -
+%               0...w - Moving average, window size w.
+%      layout - Vector of integers specifying how many outputs to plot in
+%               each figure. Number of elements in vector will correspond
+%               to the number of figures, while the value of each element
+%               in vector will correspond to the number of outputs plotted 
+%               in each figure. By default all outputs are plotted in the 
+%               same figure.
+%       scale - Values in this vector are element-wise multiplied by the
+%               corresponding outputs, and are used to scale outputs. If
+%               only one element is given, all outputs are multiplied by
+%               this element. Default is 1.
+%       iters - Number of iterations to plot (default is 0, i.e., plot all
+%               iterations).
+%      colors - Cell array of strings specifying colors with which to plot 
+%               each output within each figure. Default is 
+%               {'b', 'r', 'g', 'c', 'm', 'y', 'k'}.
 %
 % Outputs:
-%    d - Tri-dimensional matrix containing what was plotted. First 
-%        dimension corresponding to number of outputs, second dimension to
-%        number of iterations, and third dimension depends on the type of
-%        plot. If plot is of type == 'a', third dimensions corresponds to
-%        number of files, else if plot is of type == 'f', third dimension
-%        is 2 (for minima and maxima, respectively), else if plot is of
-%        type moving average, third dimension is 1 (the averaged output).
+%    d - Matrix containing what was plotted. First dimension corresponds to
+%        the number of outputs, second dimension to the number of 
+%        iterations. If plot is of type == 'a', third dimension corresponds
+%        to number of files (i.e. replications). If plot is of type == 'f', 
+%        third dimension is 2 (for minima and maxima at each iteration, 
+%        respectively). If plot is of type moving average, d only has two
+%        dimensions.
+%    h - Handles of created figures.
 %
 %
 % Copyright (c) 2015 Nuno Fachada
@@ -71,14 +86,14 @@ elseif numel(scale) == 1
     scale = scale * ones(1, num_outputs);
 end;
 
-% Default colors
-if nargin < 7
-    colors = {'b', 'r', 'g', 'c', 'm', 'y', 'k'}; 
+% Default number of iterations
+if nargin < 7 || iters == 0
+    iters = size(data, 1);
 end;
 
-% Default number of iterations
+% Default colors
 if nargin < 8
-    iters = size(data, 1);
+    colors = {'b', 'r', 'g', 'c', 'm', 'y', 'k'}; 
 end;
 
 % Initialize data vector
@@ -94,25 +109,46 @@ for i = 1:num_files
     
 end;
 
+% Number of figures to create
+num_figures = numel(layout);
+
+% Handles of created figures
+h = zeros(1, num_figures);
+
+% Create figures
+for cfig = 1:num_figures
+    h(cfig) = figure();
+    hold on;
+    grid on;    
+end;
+
+% How to plot outputs?
+
 if type == 'a' % All, superimposed
     
     % In this case, the output matrix will be all_data
     d = all_data;
     
-    % Start plotting outputs
-    i1 = 1;
-    for l = layout
+    % Cycle through the requested figures
+    i1 = 1; % First output to plot in next figure
+    for cfig = 1:num_figures
         
+        % How many outputs to plot in current figure
+        l = layout(cfig);
+        
+        % Last output to plot in next figure
         i2 = i1 + l - 1;
         
-        figure();
-        hold on;
-        grid on;
+        % Select current figure
+        figure(h(cfig));
         
+        % Cycle through replications
         for f = 1:num_files
             
+            % Cycle through outputs to plot in current figure
             for i = i1:i2
                 
+                % Plot current output
                 plot(all_data(i, 1:iters, f) * scale(i), ...
                     colors{i - i1 + 1});
     
@@ -120,12 +156,13 @@ if type == 'a' % All, superimposed
             
         end;
         
-        xlim([0 size(data, 1)]);
-        h = legend(outputs(i1:i2));
-        %set(h,'interpreter','Latex','FontSize',14);
+        % Legend and axis labels
+        xlim([0 iters]);
+        legend(outputs(i1:i2));
         xlabel('Iterations');
         ylabel('Value');
         
+        % First output to plot in next figure
         i1 = i2 + 1;
         
     end;
@@ -146,31 +183,38 @@ elseif type == 'f' % Filled
         d(i, :, 2) = max(all_data(i, :, :), [], 3);
     end;
     
-    % Plot graphs
-    i1 = 1;
-    x = 1:iters;
-    for l = layout
+    % Cycle through the requested figures (number of figures corresponds to
+    % the number of elements in variable "layout").
+    i1 = 1; % First output to plot in next figure
+    x = 1:iters; % Vector containing iterations to plot
+    for cfig = 1:num_figures
         
+        % How many outputs to plot in current figure
+        l = layout(cfig);
+        
+        % Last output to plot in next figure
         i2 = i1 + l - 1;
         
-        figure();
-        hold on;
-        grid on;
+        % Select current figure
+        figure(h(cfig));
         
+        % Cycle through outputs to plot in current figure
         for i = i1:i2
                 
+            % Plot extremes for current output
             fill_between(x, squeeze(d(i, :, 1)) * scale(i), ...
                 squeeze(d(i, :, 2)) * scale(i), ...
                 1, 'FaceColor', colors{i - i1 + 1});
     
         end;
         
-        xlim([0 size(data, 1)]);
-        h = legend(outputs(i1:i2));
-        %set(h,'interpreter','Latex','FontSize',14);
+        % Legend and axis labels
+        xlim([0 iters]);
+        legend(outputs(i1:i2));
         xlabel('Iterations');
         ylabel('Value');
         
+        % First output to plot in next figure
         i1 = i2 + 1;
         
     end;
@@ -191,29 +235,36 @@ elseif isnumeric(type) && type >= 0 % Moving average
 
     end;
     
-    % Plot graphs
-    i1 = 1;
-    for l = layout
+    % Cycle through the requested figures (number of figures corresponds to
+    % the number of elements in variable "layout").
+    i1 = 1; % First output to plot in next figure
+    for cfig = 1:num_figures
         
+        % How many outputs to plot in current figure
+        l = layout(cfig);
+        
+        % Last output to plot in next figure
         i2 = i1 + l - 1;
         
-        figure();
-        hold on;
-        grid on;
+        % Select current figure
+        figure(h(cfig));
         
+        % Cycle through outputs to plot in current figure
         for i = i1:i2
-                
+            
+            % Plot moving average for current output
             plot(d(i, :) * scale(i), ...
                 colors{i - i1 + 1});
     
         end;
         
-        xlim([0 size(data, 1)]);
-        h = legend(outputs(i1:i2));
-        %set(h,'interpreter','Latex','FontSize',14);
+        % Legend and axis labels
+        xlim([0 iters]);
+        legend(outputs(i1:i2));
         xlabel('Iterations');
         ylabel('Value');
         
+        % First output to plot in next figure
         i1 = i2 + 1;
         
     end;
